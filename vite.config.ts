7354893754609d4aeb00ab607f96e6d09ae8bc59
RajatBhaskare7 <rajatbhaskare@gmail.com>
2025-09-26 23:30:25 +0530

    updated vercel file

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,16 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+        },
+      },
+    },
+  },
 }));
